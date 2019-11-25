0a2681835291a03d2da2c77a29dbc95ad5f2ee8c
Aditya Gajbhiye <aditya.gajbhiye.20@gmail.com>
2019-11-26 02:05:52 +0530

    initial commit

---
+++ src/components/app.vue
@@ -0,0 +1,70 @@
+<template>
+<f7-app :params="f7params" >
+
+
+
+  <!-- Your main view, should have "view-main" class -->
+  <f7-view main class="safe-areas" url="/"></f7-view>
+
+</f7-app>
+</template>
+<script>
+  import cordovaApp from '../js/cordova-app.js';
+  import routes from '../js/routes.js';
+
+  export default {
+    data() {
+      return {
+        // Framework7 Parameters
+        f7params: {
+          id: 'io.framework7.myapp', // App bundle ID
+          name: 'My App', // App name
+          theme: 'auto', // Automatic theme detection
+          // App root data
+          data: function () {
+            return {
+              user: {
+                firstName: 'John',
+                lastName: 'Doe',
+              },
+
+            };
+          },
+
+          // App routes
+          routes: routes,
+
+
+          // Input settings
+          input: {
+            scrollIntoViewOnFocus: this.$device.cordova && !this.$device.electron,
+            scrollIntoViewCentered: this.$device.cordova && !this.$device.electron,
+          },
+          // Cordova Statusbar settings
+          statusbar: {
+            iosOverlaysWebView: true,
+            androidOverlaysWebView: false,
+          },
+        },
+
+        // Login screen data
+        username: '',
+        password: '',
+      }
+    },
+    methods: {
+      alertLoginData() {
+        this.$f7.dialog.alert('Username: ' + this.username + '<br>Password: ' + this.password);
+      }
+    },
+    mounted() {
+      this.$f7ready((f7) => {
+        // Init cordova APIs (see cordova-app.js)
+        if (f7.device.cordova) {
+          cordovaApp.init(f7);
+        }
+        // Call F7 APIs here
+      });
+    }
+  }
+</script>
---
+++ src/pages/home.vue
@@ -0,0 +1,22 @@
+<template>
+<f7-page name="home">
+ {{ employees }}
+</f7-page>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      employees: []
+    }
+  },
+  created() {
+    fetch('http://dummy.restapiexample.com/api/v1/employees')
+      .then(res => res.json())
+      .then(res => this.employees = res)
+      .catch(err => console.log(err));
+  }
+}
+</script>
